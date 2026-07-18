5e4f4581754304bc045352258ed9f181fd400526
CaioPelozzi <caiopelozzi18@gmail.com>
2026-07-17 21:30:09 -0300

    chore: Inicializa Spring Boot project

---
+++ src/test/java/com/bichodepatas/BichoDePatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bichodepatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BichoDePatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
